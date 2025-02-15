d67bfa72311ce2d297710cc9eaaa0827145f5fc2
esemsc-ee19d823 <timothee.boudot24@imperial.ac.uk>
2025-02-15 12:18:31 +0000

    added_products

--- model.py
+++ model.py
@@ -9,20 +9,34 @@ import random
 #         self.id = id
 #         self.salary = salary
 
+class Product:
+    def __init__(self, users, subscription_price=1):
+        self.users = users
+        self.subscription_price = subscription_price
+
+    def user_increase(self, delta_t):
+        self.users += 1 * delta_t # linear for now
+    
+    def collect_subscriptions(self):
+        return self.users * self.subscription_price
+
+
 class Employee:
     def __init__(self, id, salary):
         self.id = id
         self.salary = salary
 
-
 class Company:
-    def __init__(self, starting_employees, starting_capital, salary = 1, age=0, alive=True):
+    def __init__(self, starting_employees, starting_capital, salary = 500, age=0,
+                 alive=True, users=0, subscription_price= 1):
         self.num_employees = starting_employees
         # self.revenue_per_employee = revenue_per_employee
         self.cash = starting_capital  # Initial cash
         self.salary = salary
         self.age = age
         self.alive = alive
+        self.products = []
+        self.products.append(Product(users, subscription_price))
 
     def pay_salaries(self):
         total_paychecks = self.num_employees * self.salary
@@ -33,20 +47,21 @@ class Company:
         self.cash -= total_paychecks
         print(f'Paid {self.num_employees * self.salary} paycheck{'s' if self.num_employees > 1 else ''} in salaries')
 
+    def earn_revenue(self):
+        total_revenue = 0
+        for product in self.products:
+            total_revenue += product.collect_subscriptions()
+        self.cash += total_revenue
+        print(f'Earned {total_revenue} in revenue')
+
     def die(self):
         self.alive = False
-    # def earn_revenue(self):
-
-
-    # def hire_employee(self, salary):
-
-
-    # def fire_employee(self):
 
 class Simulation:
-    def __init__(self, company, max_steps=50):
+    def __init__(self, company, step_size=1, max_steps=50):
         self.company = company
         self.max_steps = max_steps
+        self.step_size = step_size
 
     def run(self):
         time_step=0
@@ -54,11 +69,15 @@ class Simulation:
         employees = []
         cash = []
         while company.alive and time_step < self.max_steps:
+
             employees.append(company.num_employees)
             cash.append(company.cash)
             steps.append(time_step)
+
+            company.earn_revenue()
             company.pay_salaries()
-            time_step += 1
+
+            time_step += self.step_size
             company.age += 1
         
         dead_alive = ['dead', 'alive']
@@ -66,7 +85,7 @@ class Simulation:
         return steps, employees, cash
 
 # Example Run
-company = Company(starting_employees=2, starting_capital=10)
+company = Company(starting_employees=3, starting_capital=10)
 sim = Simulation(company, max_steps=50)
 a, b, c = sim.run()
 print(c)
